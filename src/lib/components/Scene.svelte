1d46b4b91bd831a64bf7b84b7ce0aa3e3fd09878
autoc19 <en.us@outlook.com>
2025-11-24 10:57:12 +0800

    Update project with new box models and scene modifications

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { T } from '@threlte/core'
   import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import BoxExModel from './models/box_ex.svelte'
+  import BoxExNewModel from './models/box_ex_new.svelte'
+  import ShibuyaModel from './models/shibuya.svelte'
 </script>
 
 <T.PerspectiveCamera
@@ -40,42 +43,8 @@
   opacity={0.5}
 />
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
+<ShibuyaModel position={[0, 0, 0]} />
+<BoxExModel position={[4, 0, -3]} />
+<BoxExNewModel position={[-4, 0, 3]} />
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
